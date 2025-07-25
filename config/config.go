8fe96e0c0e580459aebf0ef54ebd5a590e28584b
AmryLil <ulilamry432@gmail.com>
2025-07-25 13:37:21 +0800

    imrove config db

--- config/config.go
+++ config/config.go
@@ -12,7 +12,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Makassar",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
